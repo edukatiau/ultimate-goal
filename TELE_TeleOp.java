4333552cefdcc4c7020f6f2a13b25915fd258244
edukatiau <70084379+edukatiau@users.noreply.github.com>
2021-05-11 13:59:24 -0300

    Update TELE_TeleOp.java

--- TELE_TeleOp.java
+++ TELE_TeleOp.java
@@ -8,6 +8,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -17,9 +18,10 @@ import com.qualcomm.robotcore.util.Range;
 public class TELE_TeleOp extends LinearOpMode {
   
 	//Declaração dos Objetos
-	DcMotor			frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor, Braco, Lancador;
+	DcMotor			frontLeftMotor, backLeftMotor, frontRightMotor, backRightMotor, Lancador;
+	DcMotorEx		Braco;
 	//DcMotor			Coletor;
-	Servo				Garra;
+	Servo			Garra;
 	  
 	@Override
 	public void runOpMode() {
@@ -29,16 +31,16 @@ public class TELE_TeleOp extends LinearOpMode {
 		telemetry.update();
 	
 		//Instâncias dos Objetos
+		//HUB 1
 		frontLeftMotor = hardwareMap.get(DcMotor.class, "front_left");
 		backLeftMotor = hardwareMap.get(DcMotor.class, "back_left");
 		frontRightMotor = hardwareMap.get(DcMotor.class, "front_right");
 		backRightMotor = hardwareMap.get(DcMotor.class, "back_right");
-	
-		Braco = hardwareMap.get(DcMotor.class, "braco");
-		Garra = hardwareMap.get(Servo.class, "garra");
-	
-		Lancador = hardwareMap.get(DcMotor.class, "lancador");
 		
+		//HUB 2
+		Braco = hardwareMap.get(DcMotorEx.class, "braco");
+		Garra = hardwareMap.get(Servo.class, "garra");
+		Lancador = hardwareMap.get(DcMotor.class, "lancador");
 		//Coletor = hardwareMap.get(DcMotor.class, "coletor");
 	
 		//Define a direção dos motores
@@ -57,7 +59,9 @@ public class TELE_TeleOp extends LinearOpMode {
 		backRightMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 		Braco.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 		//Coletor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
-	
+		
+		resetEncoder();
+		
 		//Informa que a inicialização obteve êxito
 		telemetry.addData("Status", "Aguardando Start");
 		telemetry.addData("Bora da-lhe", "Tchê");
@@ -69,14 +73,16 @@ public class TELE_TeleOp extends LinearOpMode {
 		double maxVelo = .5;
 		double veloBraco = 1;
 		boolean GarraOn = false;
+		int position = 0;
+		double kP = .4;
+		double BracoPower = 50;
+		boolean BracoOn = false;
 	
 		while (opModeIsActive()){
 	  
 			double y = -gamepad1.left_stick_y; //frente e trás
 			double x = gamepad1.left_stick_x; //esquerda e direita
 			double rx = gamepad1.right_stick_x; //360º no proprio eixo
-	  
-			double bracoPower = gamepad2.left_stick_y;
 	  
 			//Aumenta o Range da Velocidade
 			if(gamepad1.right_bumper){
@@ -118,23 +124,41 @@ public class TELE_TeleOp extends LinearOpMode {
 				GarraOn = !GarraOn;
 				telemetry.addData("Status","Fechando");
 				telemetry.update();
-				Garra.setPosition(0);
+				Garra.setPosition(.5);
 				sleep(500);
 			}
-	  
-			//Levantar e Descer o Braço
-			Braco.setPower(bracoPower);
+			
+			/*
+			Levantar e Descer o Braço
+			*/
+			if(gamepad2.b && BracoOn == false){
+				BracoOn = !BracoOn;
+				position = 245;
+				sleep(100);
+			}
+			if(gamepad2.b && BracoOn == true){
+				BracoOn = !BracoOn;
+				position = 0;
+				sleep(100);
+			}
+			
+			Braco.setTargetPosition(position);
+			BracoPower += 
+				(Braco.getCurrentPosition() - position)*kP;
+			
+			Braco.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+			Braco.setVelocity(BracoPower);
 			
 			/*
 			Liga o Lançador
 			*/
-			if (gamepad2.b){
+			if (gamepad2.x){
 				Lancador.setPower(1);
 			}
 			else{
 				Lancador.setPower(0);
 			}
-		
+			
 			/*
 			Código para ligar e desligar o coletor
 			
@@ -150,12 +174,20 @@ public class TELE_TeleOp extends LinearOpMode {
 			}
 			*/
 	  
-			telemetry.addData("Right Motor Power: ", frontRightMotor.getPower());
-			telemetry.addData("Left Motor Power: ", frontLeftMotor.getPower());
+			telemetry.addData("FRight Motor Power: ", frontRightMotor.getPower());
+			telemetry.addData("BRight Motor Power: ", backRightMotor.getPower());
+			telemetry.addData("BLeft Motor Power: ", backLeftMotor.getPower());
+			telemetry.addData("FLeft Motor Power: ", frontLeftMotor.getPower());
+			telemetry.addData("Target", Braco.getCurrentPosition());
+			telemetry.addData("Lancador", Lancador.getPower());
 			telemetry.addData("Velocidade Maxima", maxVelo );
 			telemetry.addData("Velocidade Minima", minVelo );
 			telemetry.update();
 	  
 		}
 	}
+	private void resetEncoder(){
+			Braco.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+			Braco.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+		}
 }
